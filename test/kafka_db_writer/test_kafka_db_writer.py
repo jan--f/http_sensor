30b50678e6dba2b0e294f0955b904d3867e4ef42
Jan <jan@fajerski.name>
2020-11-06 11:42:37 +0100

    test: add basic kafka_db_writer test
    This so far only tests that a kafka message is not committed when the db
    write failed.
    
    Signed-off-by: Jan <jan@fajerski.name>

---
+++ test/kafka_db_writer/test_kafka_db_writer.py
@@ -0,0 +1,40 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+import hypothesis.strategies as st
+
+from hypothesis import given, settings, example
+from kafka import KafkaConsumer
+
+from kafka_db_writer import kafka_db_writer
+
+
+'''
+The kafka db writer has not too many interesting parts to test. The main
+invariant for the worker is that it should only ever commit to kafka if the
+message was successfully writen to the database.
+
+Once a more flexible multithreading model is implemented, the rescheduling of
+workers would be interesting as well.
+'''
+
+
+@pytest.fixture
+def worker_config():
+    mock_config = {
+        'log': MagicMock(),
+        'kafka_config': MagicMock(topic='test'),
+        'db_config': MagicMock(),
+    }
+    return mock_config
+
+
+@patch('kafka_db_writer.kafka_db_writer.DBWriter', autospec=True)
+@patch('kafka_db_writer.kafka_db_writer.KafkaConsumer', autospec=True)
+def test_worker_no_commit_on_db_fail(mocked_kafka_consumer,
+                                     mocked_db_writer,
+                                     worker_config):
+    mocked_kafka_consumer.return_value = MagicMock(poll={'test': 'foo'})
+    mocked_db_writer.write_message.side_effect = ValueError
+    kafka_db_writer.kafka_worker(worker_config)
+    mocked_kafka_consumer.commit.assert_not_called()
